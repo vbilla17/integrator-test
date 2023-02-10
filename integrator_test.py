ba7192d9b30817b58c772c81fc2f64bf70002a6a
Vishal Billa <vbilla@usc.edu>
2023-02-09 21:00:35 -0800

    work on interpolator

--- integrator_test.py
+++ integrator_test.py
@@ -2,9 +2,9 @@ import csv
 import matplotlib.pyplot as plt
 import math
 
-def interpolate(qx, qy, qz, qw, ax, ay, az, bno_packets, accel_packets):
-    first_packet_num = min(bno_packets[0], accel_packets[0])
-    last_packet_num = max(bno_packets[-1], accel_packets[-1])
+def interpolate(qx, qy, qz, qw, ax, ay, az, bno_packets, accel_packets, bno_timestamps, accel_timestamps):
+    first_packet_num = int(min(bno_packets[0], accel_packets[0]))
+    last_packet_num = int(max(bno_packets[-1], accel_packets[-1]))
 
     qx_interpolated = []
     qy_interpolated = []
@@ -16,7 +16,8 @@ def interpolate(qx, qy, qz, qw, ax, ay, az, bno_packets, accel_packets):
     bno_timestamps_interpolated = []
     accel_timestamps_interpolated = []
 
-    for i in range(first_packet_num, last_packet_num + 1):
+    for i in range(first_packet_num, last_packet_num):
+        print("<3 ")
         # check if the current packet number is in bno_packets
         if i in bno_packets:
             idx = bno_packets.index(i)
@@ -24,7 +25,20 @@ def interpolate(qx, qy, qz, qw, ax, ay, az, bno_packets, accel_packets):
             qy_interpolated.append(qy[idx])
             qz_interpolated.append(qz[idx])
             qw_interpolated.append(qw[idx])
-            bno_timestamps_interpolated.append(i)
+            bno_timestamps_interpolated.append(bno_timestamps[idx])
+        else:
+            if bno_timestamps_interpolated:
+                qx_interpolated.append(qx_interpolated[idx - 1])
+                qy_interpolated.append(qy_interpolated[idx - 1])
+                qz_interpolated.append(qz_interpolated[idx - 1])
+                qw_interpolated.append(qw_interpolated[idx - 1])
+                bno_timestamps_interpolated.append(bno_timestamps[idx - 1])
+            else:
+                qx_interpolated.append(qx[0])
+                qy_interpolated.append(qy[0])
+                qz_interpolated.append(qz[0])
+                qw_interpolated.append(qw[0])
+                bno_timestamps_interpolated.append(bno_timestamps[0])
 
         # check if the current packet number is in accel_packets
         if i in accel_packets:
@@ -32,21 +46,23 @@ def interpolate(qx, qy, qz, qw, ax, ay, az, bno_packets, accel_packets):
             ax_interpolated.append(ax[idx])
             ay_interpolated.append(ay[idx])
             az_interpolated.append(az[idx])
-            accel_timestamps_interpolated.append(i)
-
-        # use the latest value from either bno or accel until a new value arrives
+            accel_timestamps_interpolated.append(accel_timestamps[idx])
         else:
-            if bno_timestamps_interpolated:
-                qx_interpolated.append(qx_interpolated[-1])
-                qy_interpolated.append(qy_interpolated[-1])
-                qz_interpolated.append(qz_interpolated[-1])
-                qw_interpolated.append(qw_interpolated[-1])
-                bno_timestamps_interpolated.append(i)
             if accel_timestamps_interpolated:
-                ax_interpolated.append(ax_interpolated[-1])
-                ay_interpolated.append(ay_interpolated[-1])
-                az_interpolated.append(az_interpolated[-1])
-                accel_timestamps_interpolated.append(i)
+                ax_interpolated.append(ax_interpolated[idx - 1])
+                ay_interpolated.append(ay_interpolated[idx - 1])
+                az_interpolated.append(az_interpolated[idx - 1])
+                accel_timestamps_interpolated.append(accel_timestamps[idx - 1])
+            else:
+                ax_interpolated.append(ax[0])
+                ay_interpolated.append(ay[0])
+                az_interpolated.append(az[0])
+                accel_timestamps_interpolated.append(accel_timestamps[0])
+
+        if bno_timestamps_interpolated[-1] <= accel_timestamps_interpolated[-1]:
+            bno_timestamps_interpolated[-1] = accel_timestamps_interpolated[-1]
+        else:
+            accel_timestamps_interpolated[-1] = bno_timestamps_interpolated[-1]
 
     return qx_interpolated, qy_interpolated, qz_interpolated, qw_interpolated, ax_interpolated, ay_interpolated, az_interpolated, bno_timestamps_interpolated, accel_timestamps_interpolated
 
@@ -86,13 +102,13 @@ def plotBno(qx, qy, qz, qw, bno_timestamps):
 
 def plotAcc(acc_x, acc_y, acc_z, accel_timestamps):
     fig, ax = plt.subplots(figsize=(10, 8))
-    ax.plot(acc_x[0:2300], label='acc_x')
-    ax.plot(acc_y[0:2300], label='acc_y')
-    ax.plot(acc_z[0:2300], label='acc_z')
+    ax.plot(acc_x, label='acc_x')
+    ax.plot(acc_y, label='acc_y')
+    ax.plot(acc_z, label='acc_z')
 
 def main():
-    accel_input_path = 'input/ctrl-v/ACCEL-trimmed.csv'
-    bno_input_path = 'input/ctrl-v/BNO-trimmed.csv'
+    accel_input_path = 'input/Jawbone/ACCEL-trimmed.csv'
+    bno_input_path = 'input/Jawbone/BNO-trimmed.csv'
 
     accel_timestamps = []
     bno_timestamps = []
@@ -117,13 +133,15 @@ def main():
         ay_idx = headers.index('ay')
         az_idx = headers.index('az')
         checksum_idx = headers.index('Checksum Status')
+        packet_idx = headers.index('Packet #')
 
         for row in accel_reader:
             if row[checksum_idx] == 'OK':
                 accel_timestamps.append(row[flight_ctr_idx])
-                ax.append(row[ax_idx])
-                ay.append(row[ay_idx])
-                az.append(row[az_idx])
+                ax.append(float(row[ax_idx]))
+                ay.append(float(row[ay_idx]))
+                az.append(float(row[az_idx]))
+                accel_packets.append(float(row[packet_idx]))
 
         if not (len(accel_timestamps) == len(ax) == len(ay) == len(az)):
             print("Acceleration and timestamp lists are different lengths!!")
@@ -142,16 +160,18 @@ def main():
         quat_z_idx = headers.index('quatZ')
         quat_w_idx = headers.index('quatW')
         checksum_idx = headers.index('Checksum Status')
+        packet_idx = headers.index('Packet #')
 
         for row in bno_reader:
             if row[checksum_idx] == 'OK':
                 bno_timestamps.append(row[flight_ctr_idx])
-                quat_x.append(float(row[quat_x_idx]) / math.sqrt(1 - pow(float(row[quat_w_idx]), 2)))
-                quat_y.append(float(row[quat_y_idx]) / math.sqrt(1 - pow(float(row[quat_w_idx]), 2)))
-                quat_z.append(float(row[quat_z_idx]) / math.sqrt(1 - pow(float(row[quat_w_idx]), 2)))
+                quat_x.append(float(row[quat_x_idx]))
+                quat_y.append(float(row[quat_y_idx]))
+                quat_z.append(float(row[quat_z_idx]))
                 quat_w.append(float(row[quat_w_idx]))
-                sum = (float(row[quat_w_idx]) * float(row[quat_w_idx])) + (float(row[quat_x_idx]) * float(row[quat_x_idx])) + (float(row[quat_y_idx]) * float(row[quat_y_idx])) + (float(row[quat_z_idx]) * float(row[quat_z_idx]))
-                quat_sum_sq.append(sum)
+                bno_packets.append(float(row[packet_idx]))
+                # sum = (float(row[quat_w_idx]) * float(row[quat_w_idx])) + (float(row[quat_x_idx]) * float(row[quat_x_idx])) + (float(row[quat_y_idx]) * float(row[quat_y_idx])) + (float(row[quat_z_idx]) * float(row[quat_z_idx]))
+                # quat_sum_sq.append(sum)
 
         if not (len(bno_timestamps) == len(quat_x) == len(quat_y) == len(quat_z) == len(quat_w)):
             print("BNO quat and timestamp lists are different lengths!!")
@@ -163,6 +183,11 @@ def main():
     print("BNO length: ", len(bno_timestamps))
 
     plotBno(quat_x, quat_y, quat_z, quat_w, bno_timestamps)
+
+    quat_x, quat_y, quat_z, quat_w, ax, ay, az, bno_timestamps, accel_timestamps = interpolate(quat_x, quat_y, quat_z, quat_w, ax, ay, az, bno_packets, accel_packets, bno_timestamps, accel_timestamps)
+
+    print("Interpolated Accel length: ", len(accel_timestamps))
+    print("Interpolated BNO length: ", len(bno_timestamps))
 
 if __name__ == "__main__":
     main()
